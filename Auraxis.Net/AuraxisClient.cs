47910116efca0c62500347b4981c109b6ddc3f86
Furkan Kambay <furkankambay@outlook.com>
2019-04-30 15:27:06 +0300

    :bug: Fix collection names

--- Auraxis.Net/AuraxisClient.cs
+++ Auraxis.Net/AuraxisClient.cs
@@ -3,6 +3,7 @@ using Flurl;
 using Flurl.Http;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Auraxis.Net
@@ -11,25 +12,31 @@ namespace Auraxis.Net
     {
         public static async Task<List<T>> GetAsync<T>(Platform platform, int limit = 1)
         {
-            var collectionName = typeof(T).Name.ToLowerInvariant();
+            (var collectionName, var listName) = GetCollectionName<T>();
 
             Url url = ApiUtilities.GetGetUrl(platform)
                 .AppendPathSegment(collectionName)
                 .SetQueryParam("c:limit", limit);
 
             JObject result = await url.GetJsonAsync<JObject>().ConfigureAwait(false);
-            return result[$"{collectionName}_list"].ToObject<List<T>>();
+            return result[listName].ToObject<List<T>>();
         }
 
         public static async Task<int> CountAsync<T>(Platform platform)
         {
-            var collectionName = typeof(T).Name.ToLowerInvariant();
+            (var name, _) = GetCollectionName<T>();
 
             Url url = ApiUtilities.GetCountUrl(platform)
-                .AppendPathSegment(collectionName);
+                .AppendPathSegment(name);
 
             JObject result = await url.GetJsonAsync<JObject>().ConfigureAwait(false);
             return result.First.ToObject<int>();
+        }
+
+        private static (string name, string listName) GetCollectionName<T>()
+        {
+            var name = Regex.Replace(typeof(T).Name, "(?!^)([A-Z0-9])", "_$1").ToLowerInvariant();
+            return (name, $"{name}_list");
         }
     }
 }
